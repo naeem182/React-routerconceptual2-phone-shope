a68ca9c147965ecb7c5d5698bf69301c8ceb7106
naeem <naeemislamjr182@gmail.com>
2023-09-24 01:30:53 +0600

    set up

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h2>main layout</h2>
+        </div>
+    )
+}
+
+export default MainLayout
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import MyCreatedRouter from './Router/Route'
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={MyCreatedRouter}></RouterProvider>
+  </React.StrictMode>,
+)
