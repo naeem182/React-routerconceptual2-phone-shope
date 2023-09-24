2bb746babd3f528bb8e5bb3d908a60f3fe4a6291
naeem <naeemislamjr182@gmail.com>
2023-09-24 17:43:19 +0600

    to show cart from local storage

--- src/Pages/Home/Favorite/Favorite.jsx
+++ src/Pages/Home/Favorite/Favorite.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react"
+import PhoneCard from "../../../Components/Phone/PhoneCard";
 
 
 const Favorite = () => {
+
+    const [favorite, setfavorite] = useState([]);
+    const [notfound, setnotfound] = useState(false)
+
+    useEffect(() => {
+        const favoriteIteam = JSON.parse(localStorage.getItem('favorite'));
+        if (favoriteIteam) {
+            setfavorite(favoriteIteam)
+        } else {
+            setnotfound("No data found");
+        }
+    }, [])
+
     return (
         <div className=" container mx-auto">
-            <h2>favorites</h2>
+            {
+                notfound ? <p className="h-[80vh] flex justify-center items-center">{notfound}</p> :
+                    <div className="grid grid-cols-2 gap-5">
+                        {favorite.map(phone => <PhoneCard key={phone.id} phone={phone}></PhoneCard>)}
+                    </div>
+            }
         </div>
     )
 }
